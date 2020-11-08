dd586d5167dfd6d4c67c5dc0e712dda87d5b25a9
Caroline Goulart <goulartscaroline@gmail.com>
2020-11-08 16:15:53 -0300

    Created GoBarber API

---
+++ gobarber-api/src/services/AuthenticateUserService.ts
@@ -0,0 +1,57 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: String;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    // user.password - Encrypted passoword (from database)
+    // password - Unencrypted passoword (provided by the user in the login)
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    // generate the token that will be returned to the client
+    // 1st param: payload (encrypted but not secured, because it can be decrypted)
+    // 2nd param: secret-key (key that is used to generate the token)
+    // 3rd param: token configs (subject: user id from the user that generated the token)
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
